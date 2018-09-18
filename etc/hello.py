8c60c0d9df6d2a3c34477a246e9475f9ec39cd7c
tihiy <alexanderser@yandex.ru>
2018-09-18 17:11:03 +0300

    Django start

--- etc/hello.py
+++ etc/hello.py
@@ -1,3 +1,2 @@
 bind = "0.0.0.0:8080"
-use = "/home/box/web/hello:wsgi_application"
 
--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/django_site.py /etc/gunicorn.d/django_site.py
+cd /home/box/web/
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
+cd /home/box/web/ask/
+sudo gunicorn -c /etc/gunicorn.d/django_site.py ask.wsgi:application
 sudo /etc/init.d/mysql restart
