5e856e5d306a97d4dad5b2607aedcbf71637b36a
tihiy <alexanderser@yandex.ru>
2018-09-19 14:01:12 +0300

    DB task

--- init.sh
+++ init.sh
@@ -2,10 +2,11 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/django_site.py /etc/gunicorn.d/django_site.py
-cd /home/box/web/
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
-cd /home/box/web/ask/
-sudo gunicorn -c /etc/gunicorn.d/django_site.py ask.wsgi:application
 sudo /etc/init.d/mysql restart
+mysql -u root -e "create database qa_db;"
+cd /home/box/web/ask/
+python3 manage.py makemigrations
+python3 manage.py migrate
+sudo gunicorn -c /etc/gunicorn.d/django_site.py ask.wsgi:application
+
